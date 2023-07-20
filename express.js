374c3bf5fbf60f8d8b25f3eb5d567aa73b583023
Andri Möll <andri@dot.ee>
2023-07-20 10:04:16 +0000

    Enable require extension in Express plugin if missing.

--- express.js
+++ express.js
@@ -1,3 +1,5 @@
+if (require.extensions[".jsx"] == null) void require("j6pack/register")
+
 module.exports = function(path, attrs, done) {
 	// NOTE: Express expects the view engine to cache compiled views. Jade, for
 	// example, keeps a global cache on Jade.cache. We're depending on the Node
